d2ce0e3238442e8934006176afe2c5c75d2df422
nino <ninopaunov93@outlook.com>
2022-04-17 15:55:27 +0200

    add switch case

--- main.js
+++ main.js
@@ -2,30 +2,49 @@ function uporedi() {
     let output = document.querySelector('#output')
     let firstNum = document.querySelector('#firstNum').value
     let secondNum = document.querySelector('#secondNum').value
+    let thirdNum = document.querySelector('#thirdNum').value
 
     firstNum = parseInt(firstNum)
     secondNum = parseInt(secondNum)
+    thirdNum = parseInt(thirdNum)
 
-    if (firstNum > secondNum) {
-        output.innerHTML = ' prvi broj je veci'
+    switch(true) {
+        case (firstNum > secondNum && firstNum > thirdNum):
+            output.innerHTML = ' prvi broj je najveci'
+            break;
+
+        case (secondNum > firstNum && secondNum > thirdNum):
+                output.innerHTML = ' drugi broj je najveci'
+            break;
+
+        case (thirdNum > firstNum && thirdNum > secondNum):
+            output.innerHTML = ' treci broj je najveci'
+            break;
+
     }
-    else if (secondNum > firstNum) {
-        output.innerHTML = ' drugi broj je veci'
-    }
-    else {
-        output.innerHTML = ' brojevi su jednaki'
-    }
+
+
+    // if (firstNum > secondNum ) {
+    //     output.innerHTML = ' prvi broj je najveci'
+    // }
+    // else if (secondNum > firstNum ) {
+    //     output.innerHTML = ' drugi broj je najveci'
+    // } 
+    // else {
+    //     output.innerHTML = ' brojevi su jednaki'
+    //     // output.innerHTML = 'treci broj je najveci'
+    // }
 }
 
-function saberi(){
+function saberi() {
     let ukupno = document.querySelector('#ukupno')
     let firstNum = document.querySelector('#firstNum').value
     let secondNum = document.querySelector('#secondNum').value
-    
+
     firstNum = parseInt(firstNum)
     secondNum = parseInt(secondNum)
-    
-    let izracunato = firstNum + secondNum 
+
+    let izracunato = firstNum + secondNum
     ukupno.innerHTML = `ukupno je ${izracunato}`
 
 
